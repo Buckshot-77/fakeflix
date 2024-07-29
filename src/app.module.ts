c33609a486e02fc46f7338958e3783ac344b4583
Hugo Oliveira <hugo684@gmail.com>
2024-07-29 16:43:57 -0300

    chore: reorganize imports for content controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { ContentController } from '@src/http/controller/content.controller';
+import { ContentController } from '@src/http/rest/controller/content.controller';
 import { ContentManagementService } from '@src/core/service/content-management.service';
+import { MediaPlayerService } from '@src/core/service/media-player.service';
 
 import { PrismaService } from '@src/prisma.service';
 
 @Module({
   imports: [],
   controllers: [ContentController],
-  providers: [ContentManagementService, PrismaService],
+  providers: [ContentManagementService, MediaPlayerService, PrismaService],
 })
 export class AppModule {}
