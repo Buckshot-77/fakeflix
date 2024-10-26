1ab57ed422d8c68eeed8c6e9585385dcbb475e0e
Hugo Oliveira <hugooliveira.dev@proton.me>
2024-10-26 13:48:53 -0300

    feat: add default entity for typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { MediaPlayerController } from '@src/http/rest/controller/media-player.co
 import { ContentManagementService } from '@src/core/service/content-management.service';
 import { MediaPlayerService } from '@src/core/service/media-player.service';
 
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
 import { ContentRepository } from '@src/persistence/repository/content.repository';
 import { VideoRepository } from '@src/persistence/repository/video.repository';
 
@@ -18,7 +17,6 @@ import { ConfigModule } from '@src/infra/module/config/config.module';
   providers: [
     ContentManagementService,
     MediaPlayerService,
-    PrismaService,
     ContentRepository,
     VideoRepository,
   ],
