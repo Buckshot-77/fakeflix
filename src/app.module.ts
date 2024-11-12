06455b0ee6d9b7c194fe42b019bc027d1e07fe45
Hugo Oliveira <hugooliveira.dev@proton.me>
2024-11-12 17:14:55 -0300

    feat: add correct imports to app module and fix jest config file to consider identity module tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
-import { ContentModule } from '@contentModule/content.module';
 import { Module } from '@nestjs/common';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { ContentModule } from '@contentModule/content.module';
+import { IdentityModule } from '@identityModule/identity.module';
 
 @Module({
-  imports: [ContentModule],
+  imports: [
+    ContentModule,
+    IdentityModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
 })
 export class AppModule {}
